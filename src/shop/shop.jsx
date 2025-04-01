48778f9866fc54cd1d763194c75b1659a1ca79ea
Lmannumb <leifcn@byu.edu>
2025-04-01 00:07:13 -0500

    A little bit for the shop

--- src/shop/shop.jsx
+++ src/shop/shop.jsx
@@ -4,15 +4,20 @@ import plant from '/exampleplant.png';
 
 export function Shop() {
   const [sales, setSales] = React.useState([]);
+  const [, forceUpdate] = React.useReducer(x => x + 1, 0)
 
   React.useEffect(()=>{
     localStorage.setItem("shop", JSON.stringify([{
-      cost: 98,
-      worth: 800,
-      name: "Basic Plant",
-      image: plant,
-      time: "2:00:00"
+      item: {
+        cost: 98,
+        worth: 800,
+        name: "Basic Plant",
+        image: plant,
+        time: "2:00:00"
+      },
+      available: true
     }]));
+    
     const shopString = localStorage.getItem('shop');
     if (shopString) {
       console.log("offers: " + shopString);
@@ -28,30 +33,33 @@ export function Shop() {
       function buy() {
         const index = j;
         return function () {
-          const item = sales[index];
-          sales.splice(index, 1);
+          const item = sales[index].item;
+          sales[index].available = false;
           const a = JSON.parse(localStorage.getItem("username"+"garden"));
           a.push(item);
           localStorage.setItem("username"+"garden", JSON.stringify(a));
+          setSales(sales);
+          console.log("buy " + index);
+          forceUpdate();
         }
       }
       shoparray.push(
-        <div><h3>{i.name}</h3></div>
+        <div><h3 style={i.available ? {} : {color : "gray"}}>{i.item.name}</h3></div>
       );
       shoparray.push(
-        <div><img src={i.image} alt="plant for sale did not load!"></img></div>
+        <div><img src={i.item.image} alt="plant for sale did not load!"></img></div>
       );
       shoparray.push(
-        <div>{"Seed Cost: $"+i.cost}</div>
+        <div style={i.available ? {} : {color : "gray"}}>{"Seed Cost: $"+i.item.cost}</div>
       );
       shoparray.push(
-        <div>{"Plant Worth: $"+i.worth}</div>
+        <div style={i.available ? {} : {color : "gray"}}>{"Plant Worth: $"+i.item.worth}</div>
       );
       shoparray.push(
-        <div>{"Grow Time: "+i.time}</div>
+        <div style={i.available ? {} : {color : "gray"}}>{"Grow Time: "+i.item.time}</div>
       );
       shoparray.push(
-        <Button variant="primary" onClick={buy()}>Buy</Button>
+        <Button variant="primary" disabled={!i.available} onClick={buy()}>Buy</Button>
       );
       j++;
     }
