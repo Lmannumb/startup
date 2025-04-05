a2e1cfd85379a0ce743cfe9e720dfd7ed22c4349
Lmannumb <leifcn@byu.edu>
2025-04-05 06:42:15 -0500

    Just a little more inspiration from Simon. Anyways, the authentication finally works now. By the way, the guide for Simon on the website is filled to the brim with little syntax errors that took me six hours and ChatGPT to solve lol. (I might also just have a funny machine)

--- service/index.js
+++ service/index.js
@@ -4,17 +4,105 @@ const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const app = express();
 
+const authCookieName = 'token';
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
 let users = [];
-//let scores = [];
 
 let apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+apiRouter.post('/auth/create', async (req, res) => {
+  //console.log(req);
+  //console.log(req.body);
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
 
-app.get('*balls', (_req, res) => {
-  res.send({ msg: 'Pgarden service' });
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
 });
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+apiRouter.get('/scores', verifyAuth, (_req, res) => {
+  res.send(scores);
+});
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
