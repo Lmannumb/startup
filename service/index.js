ddce65faf89c1ae2f45ece102d99e37c3b2cdd0f
Lmannumb <leifcn@byu.edu>
2025-04-08 05:08:44 -0500

    DB tester

--- service/index.js
+++ service/index.js
@@ -4,6 +4,20 @@ const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const app = express();
 
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
 
 const authCookieName = 'token';
 
